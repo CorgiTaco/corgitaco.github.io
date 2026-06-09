30b961d334b01cab6d19fbc6376aa35e105d1e0e
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2026-06-09 13:16:33 -0700

    WIP Mobile Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,58 @@ document.addEventListener("DOMContentLoaded", () => {
         waveSpeed: 0.65,
         zoom: 0.75
     });
+
+    function calcWidth() {
+        var $nav = $('#nav-main');
+        var $more = $nav.find('.more');
+
+        // 1. Temporarily disable flex-grow so we can measure the *true* minimum widths
+        $nav.addClass('measuring');
+
+        // Reserve space for the "More" button (using its inline data-width fallback if hidden)
+        var morewidth = $more.outerWidth(true) || $more.data('width') || 80;
+        // REPLACE IT WITH THESE 3 LINES:
+        var headerWidth = $('.header').innerWidth();
+        var titleWidth = $('.page-title').outerWidth(true);
+        var availablespace = headerWidth - titleWidth - morewidth - 30; // 30px safety buffer prevents overlap
+        var navwidth = 0;
+        $nav.children('li:not(.more)').each(function() {
+            navwidth += $(this).outerWidth(true);
+        });
+
+        // 2. Move overflowing items to the dropdown
+        while (navwidth > availablespace && $nav.children('li:not(.more)').length > 0) {
+            var lastItem = $nav.children('li:not(.more)').last();
+            lastItem.attr('data-width', lastItem.outerWidth(true)); // Store intrinsic width
+            lastItem.prependTo($more.find('ul'));
+
+            // Recalculate navwidth by subtracting the removed item
+            navwidth -= lastItem.data('width');
+        }
+
+        // 3. Bring items back if there is enough available space
+        while ($more.find('li').length > 0) {
+            var firstMoreElement = $more.find('li').first();
+            if (navwidth + firstMoreElement.data('width') < availablespace) {
+                firstMoreElement.insertBefore($more);
+                navwidth += firstMoreElement.data('width');
+            } else {
+                break; // Stop loop if the next item doesn't fit
+            }
+        }
+
+        // 4. Toggle the "More" button visibility
+        if ($('.more li').length > 0) {
+            $('.more').css('display', 'block'); // Block aligns better with flex items
+        } else {
+            $('.more').css('display', 'none');
+        }
+
+        // 5. Re-enable flex-grow so the visible items dynamically stretch to fill all empty space
+        $nav.removeClass('measuring');
+    }
+
+    $(window).on('resize load',function(){
+        calcWidth();
+    });
 });
